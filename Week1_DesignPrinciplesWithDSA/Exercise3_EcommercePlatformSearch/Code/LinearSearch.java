92efbcd5eb2a1bd14ff3823e215661d49cc1f402
Chittaranjan Mohanty <161131185+CHITTARANJANMOHANTY-2003@users.noreply.github.com>
2025-06-30 21:14:08 +0530

    files are added.

---
+++ Week1_DesignPrinciplesWithDSA/Exercise3_EcommercePlatformSearch/Code/LinearSearch.java
@@ -0,0 +1,13 @@
+package EcommercePlatformSearch;
+
+public class LinearSearch {
+    public static Product search(Product[] products, String targetName) {
+        for (Product product : products) {
+            if (product.productName.equalsIgnoreCase(targetName)) {
+                return product;
+            }
+        }
+        return null;
+    }
+}
+
--- Week1_DesignPrinciplesWithDSA/Exercise3_EcommercePlatformSearch/Code/Main.java
+++ Week1_DesignPrinciplesWithDSA/Exercise3_EcommercePlatformSearch/Code/Main.java
@@ -1,1 +1,26 @@
+package EcommercePlatformSearch;
+public class Main {
+    public static void main(String[] args) {
+        Product[] products = {
+            new Product(101, "Laptop", "Electronics"),
+            new Product(102, "Shampoo", "Health"),
+            new Product(103, "Keyboard", "Electronics"),
+            new Product(104, "Shoes", "Fashion"),
+            new Product(105, "Book", "Education")
+        };
+        String target = "Shoes";
+        // Linear Search
+        long start1 = System.nanoTime();
+        Product result1 = LinearSearch.search(products, target);
+        long end1 = System.nanoTime();
+        System.out.println("Linear Search Result: " + result1);
+        System.out.println("Linear Search Time: " + (end1 - start1) + " ns");
+        // Binary Search
+        long start2 = System.nanoTime();
+        Product result2 = BinarySearch.search(products, target);
+        long end2 = System.nanoTime();
+        System.out.println("Binary Search Result: " + result2);
+        System.out.println("Binary Search Time: " + (end2 - start2) + " ns");
+    }
+}
 
